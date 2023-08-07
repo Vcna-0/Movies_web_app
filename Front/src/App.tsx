538153655253d7f35a91a8ba354d6194a6418907
Noëmie <ncav@protonmail.com>
2023-08-07 15:55:23 +0200

    first commit

---
+++ Front/src/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import './App.css'
+
+function App() {
+  return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Home/> } />
+                <Route path="/login" element={ <Login/> } />
+                <Route path="/signup" element={ <Signup/> } />
+            </Routes>
+        </BrowserRouter>
+  )
+}
+
+export default App
