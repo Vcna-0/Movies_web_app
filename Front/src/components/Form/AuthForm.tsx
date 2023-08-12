5f7a5709a04a51d1271ab574ed897a36a6835c5d
Noëmie <ncav@protonmail.com>
2023-08-13 01:08:45 +0200

    style: switch to mobile-first design and add responsive support
    - Changed styles from desktop-first to mobile-first approach
    - Added responsive design for mobile, tablet, and desktop views
    - Refactored code for improved readability and maintainability

--- Front/src/components/Form/AuthForm.tsx
+++ Front/src/components/Form/AuthForm.tsx
@@ -1,7 +1,8 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import axios from "axios";
 import { useState } from "react";
-import { StyledButton, StyledForm, StyledInput, StyledLoginPrompt, StyledLoginLink, StyledErrorText } from "./AuthFormStyles.tsx";
+import { StyledButton, StyledForm, StyledInputsContainer, StyledInput, StyledLoginPrompt, StyledLoginLink, StyledErrorText } from "./AuthFormStyles.tsx";
+import { StyledTitle } from "../../styles/globalStyle.tsx";
 import { useNavigate } from "react-router-dom";
 
 type Inputs = {
@@ -43,20 +44,24 @@ const AuthForm = ({ mode }: Props) => {
     const password = watch("password", "");
     const promptText = mode === "signup" ? "Already have an account? " : "Don't have an account? ";
     const linkText = mode === "signup" ? "Log in" : "Sign up";
+    const TitleText = mode === "signup" ? "Sign Up" : "Login";
 
     return (
             <StyledForm onSubmit={handleSubmit(onSubmit)}>
-                <StyledInput type="email" {...register("email", { required: true })} placeholder="Email adress"/>
-                <StyledInput type="password" {...register("password", { required: true })} placeholder="Password"/>
-                {mode === "signup" && (
-                    <>
-                        <StyledInput type="password" placeholder="Repeat password" {...register("confirmPassword", {
-                            required: true,
-                            validate: value => value === password || "Passwords do not match"
-                        })}/>
-                        {isSubmitted && errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
-                    </>
-                )}
+                <StyledTitle>{ TitleText }</StyledTitle>
+                <StyledInputsContainer>
+                    <StyledInput type="email" {...register("email", { required: true })} placeholder="Email adress"/>
+                    <StyledInput type="password" {...register("password", { required: true })} placeholder="Password"/>
+                    {mode === "signup" && (
+                        <>
+                            <StyledInput type="password" placeholder="Repeat password" {...register("confirmPassword", {
+                                required: true,
+                                validate: value => value === password || "Passwords do not match"
+                            })}/>
+                            {isSubmitted && errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+                        </>
+                    )}
+                </StyledInputsContainer>
                 <StyledButton type="submit" value={mode === 'signup' ? "Create an account" : "Log in"}/>
                 <StyledLoginPrompt>{ promptText }<StyledLoginLink href={mode === 'signup' ? '/login' : '/signup'}>{linkText}</StyledLoginLink></StyledLoginPrompt>
                 {apiError && <StyledErrorText>{apiError}</StyledErrorText>}
