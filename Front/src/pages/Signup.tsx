d3cd3b2b95ae4953e0f507d1f1969e0e723d216c
Noëmie <ncav@protonmail.com>
2023-08-15 21:18:40 +0200

    Refactor: Remove the menu component from the signup page

--- Front/src/pages/Signup.tsx
+++ Front/src/pages/Signup.tsx
@@ -2,12 +2,9 @@ import AuthForm from "../components/Form/AuthForm.tsx";
 import logo from "../assets/MovieIcon.svg";
 import { StyledMainForm } from "../components/Form/AuthFormStyles.tsx";
 
-import Menu from "../components/menu/Menu";
-
 const Signup = () => {
     return (
         <StyledMainForm>
-            <Menu />
             <img src={logo} alt="" />
             <AuthForm mode="signup"/>
         </StyledMainForm>
